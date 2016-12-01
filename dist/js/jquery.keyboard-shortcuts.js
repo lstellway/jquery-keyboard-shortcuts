a73388fd845a3ffdab5a2a61fbaa0cb8a834a4b2
Logan Stellway <logan@gigasavvy.com>
2016-11-30 18:04:49 -0800

    Added RequireJS factory pattern

--- dist/js/jquery.keyboard-shortcuts.js
+++ dist/js/jquery.keyboard-shortcuts.js
@@ -1,4 +1,12 @@
-(function($){
+(function (factory) {
+  if (typeof define === 'function' && define.amd) {
+    // AMD. Register as an anonymous module depending on jQuery.
+    define(['jquery'], factory);
+  } else {
+    // No AMD. Register plugin with global jQuery object.
+    factory(jQuery);
+  }
+}(function ($) {
     $.fn.initKeyboard = function(o) {
         // Scope
         var s = {
@@ -87,4 +95,4 @@
 
         return s.el;
     };
-})(jQuery);
+}));
